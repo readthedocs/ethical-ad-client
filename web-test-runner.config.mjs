401d67d582cfd9764a4ca4cfabaea373f823fec9
Anthony Johnson <aj@ohess.org>
2023-10-31 12:25:44 -0700

    Not headless, just a different user agent. Avoid block at CF

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -33,7 +33,7 @@ export default {
   browsers: [
     chromeLauncher({
       launchOptions: {
-        headless: false,
+        args: ["--user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'"],
       },
     }),
   ],
