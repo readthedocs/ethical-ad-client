44c10dd3450572eabe5371f4a05e6de6072e1006
Anthony Johnson <aj@ohess.org>
2023-10-31 12:27:11 -0700

    Format

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -33,7 +33,9 @@ export default {
   browsers: [
     chromeLauncher({
       launchOptions: {
-        args: ["--user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'"],
+        args: [
+          "--user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'",
+        ],
       },
     }),
   ],
