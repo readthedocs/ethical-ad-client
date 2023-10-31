08a37d7bc1807d7ea1352b0f89e7a430e7831f00
David Fischer <djfische@gmail.com>
2023-10-31 15:12:55 -0700

    Add a keyword detection test

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -30,13 +30,4 @@ export default {
       requireReturnsDefault: "preferred",
     }),
   ],
-  browsers: [
-    chromeLauncher({
-      launchOptions: {
-        args: [
-          "--user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'",
-        ],
-      },
-    }),
-  ],
 };
